cb58cca2add9ed30b9baefafb60c75a1341dc26f
lambertco <blambertco@gmail.com>
2019-10-02 22:29:16 -0600

    updated method on Word

--- Word.js
+++ Word.js
@@ -4,7 +4,7 @@ function Word(word) {
     this.word = word;
     this.letters = [];
 
-    this.makeLetters = function() {
+    this.makeWords = function() {
         var wordArr = this.word.split("");
         for(var i = 0; i < wordArr.length; i++) {
             var newLetter = new Letter(wordArr[i]);
