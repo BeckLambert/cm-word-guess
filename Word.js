7eb8f80c56e6af30c50e5c7feecb7ec861ffe22b
lambertco <blambertco@gmail.com>
2019-10-02 22:27:38 -0600

    some logic written for Word, so far no errors

--- Word.js
+++ Word.js
@@ -1,3 +1,27 @@
 var Letter = require("./Letter");
 
+function Word(word) {
+    this.word = word;
+    this.letters = [];
+
+    this.makeLetters = function() {
+        var wordArr = this.word.split("");
+        for(var i = 0; i < wordArr.length; i++) {
+            var newLetter = new Letter(wordArr[i]);
+            this.letters.push(newLetter);
+        }
+    }
+
+    this.guesses = function(guess) {
+        this.letters.forEach(letter => {
+            letter.checkLetter(guess);
+        });
+    }
+}
+
+
+
+
+
+
 module.exports = Word;
